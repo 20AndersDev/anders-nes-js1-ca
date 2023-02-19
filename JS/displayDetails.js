681ebbc21b4127677c980abcf57b790cea05638a
Noroff-Anders <AndNes16878@stud.noroff.no>
2023-02-19 14:55:14 +0100

    adding image and stuff

--- JS/displayDetails.js
+++ JS/displayDetails.js
@@ -1,6 +1,7 @@
 const itemName = new URL(window.location.href).searchParams.get("name");
 const itemProperties = document.querySelector("#item-properties");
 const itemNameEl = document.querySelector("#item-name");
+const itemImage = document.querySelector("#item-image")
 const apiUrl = `https://botw-compendium.herokuapp.com/api/v2/entry/${itemName}`;
 
 async function displayDetails() {
@@ -11,13 +12,23 @@ async function displayDetails() {
     itemNameEl.textContent = itemData.data.name;
 
     Object.keys(itemData.data).forEach(property => {
+      if(property === "image" || property === "name") {
+        return;
+      }
       const li = document.createElement("li");
       li.textContent = `${property}: ${itemData.data[property]}`;
       itemProperties.appendChild(li);
+
+      
+
     });
+
+    itemImage.src = `https://botw-compendium.herokuapp.com/api/v2/entry/${itemName}/image`;
     document.querySelector("#loader").style.display = "none"
   } catch (error) {
-    itemNameEl.textContent = `An error occurred: ${error.message}`;
+    document.querySelector("#loader").style.display = "none"
+    const errorMessage = document.querySelector("#error-message");
+    errorMessage.textContent = `An error occurred: ${error.message}`;
   }
 }
 
